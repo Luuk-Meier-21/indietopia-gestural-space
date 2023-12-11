f036128ccefa80db44419476c48b24b6e0b2a97c
Luuk Meier <luuk@pixelpillow.nl>
2023-12-11 11:58:49 +0100

    Rollup fix

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ExternalType, logExternal } from "external";
+import { ExternalType, logExternal } from "./external";
 
 logExternal(ExternalType.FOO, 4);
 logExternal(ExternalType.FOO, 2);
